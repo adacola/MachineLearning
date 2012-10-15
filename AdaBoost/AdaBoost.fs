7860d1d744819d17f676e1a4de02451f48633203
adacola <adacola@le.to>
2012-10-15 11:30:57 +0900

    AdaBoost.fsを修正

--- AdaBoost/AdaBoost.fs
+++ AdaBoost/AdaBoost.fs
@@ -5,59 +5,61 @@ type Label = private Label of int with
     static member Create = function 1 -> Label 1 | -1 -> Label -1 | _ -> invalidArg "value" "1または-1を指定してください。"
     static member CreateFromSign labelIfZero = function 0 -> labelIfZero | value -> Label.Create value
     static member ToInt = function Label x -> x
+    static member IsPositive = function Label 1 -> true | _ -> false
 let (|Label|) = Label.ToInt
-
-/// 分類器
-[<ReferenceEquality>]
-type Hypothesis<'example> = {
-    /// 事象からラベルを求める関数
-    Evaluate : 'example -> Label
-}
 
 open FSharp.Control
 
-/// 教師データと弱分類器のリストから仮説を生成し、検証データを評価する
-let evaluate trainingDatum weakHypothesises =
-    /// 教師データの件数
-    let trainingDatumCount = trainingDatum |> List.length
-
+/// 弱分類器と訓練データから仮説を生成し、検証データを評価する
+let evaluate weakClassifiers trainingDatum =
     /// 弱分類器のリストから最良の弱分類器とそのエラー率を抽出する
-    let getBestHypothesis trainingDatum distributions =
-        let getError trainingDatum distributions hypothesis =
+    let getBestClassifier trainingDatum distributions =
+        /// 期待するラベルと弱分類器の結果のラベルが異なっている場合は1を、等しい場合は0を返す
+        let isError (Label expectedLabel) (Label actualLabel) = (expectedLabel + 1) ^^^ (actualLabel + 1) >>> 1
+        /// 弱分類器の重み付けされたエラー率を求める
+        let getTotalError trainingDatum distributions classifier =
             Seq.zip trainingDatum distributions
-            |> Seq.sumBy (fun ((data, Label label), distribution) ->
-                (Label.ToInt (hypothesis.Evaluate data) - label) / 2 |> abs |> float |> (*) distribution)
-        AsyncSeq.ofSeq >> AsyncSeq.mapAsync (fun hypothesis -> async { return hypothesis, getError trainingDatum distributions hypothesis })
+            |> Seq.sumBy (fun ((data, label), distribution) -> classifier data |> isError label |> float |> (*) distribution)
+        AsyncSeq.mapAsync (fun classifier -> async { return classifier, getTotalError trainingDatum distributions classifier })
         >> AsyncSeq.toBlockingSeq >> Seq.minBy snd
 
     /// 弱分類器のエラー率から、その弱分類器に対する重みを求める
-    let getWeightOfHypothesis error = (1.0 - error) / error |> log |> (*) 0.5
+    let getWeightOfClassifier error = (1.0 - error) / error |> log |> (*) 0.5
 
-    /// 標本に対する重みの次の値を求める
-    let getNextDistributions hypothesis weight trainingDatum distributions =
+    /// 標本に対する重みの次のリストを求める
+    let getNextDistributions classifier weight trainingDatum distributions =
+        /// 正規化されていない重みの次のリスト
         let denormalizedNextDistributions =
             (AsyncSeq.ofSeq trainingDatum, AsyncSeq.ofSeq distributions) ||> AsyncSeq.zip
             |> AsyncSeq.mapAsync (fun ((data, Label label), distribution) ->
-                async { return label * Label.ToInt (hypothesis.Evaluate data) |> float |> (*) -weight |> exp |> (*) distribution })
+                async { return label * Label.ToInt (classifier data) |> float |> (*) -weight |> exp |> (*) distribution })
+        /// 重みを正規化するための除数
         let normalizationFactor = denormalizedNextDistributions |> AsyncSeq.toBlockingSeq |> Seq.sum
+        // 正規化された重みの次のリストを求める
         denormalizedNextDistributions |> AsyncSeq.mapAsync (fun nextDistribution -> async { return nextDistribution / normalizationFactor })
         |> AsyncSeq.toBlockingSeq
     
     /// 訓練データから、有効な弱分類器とその重みのリストを求める
-    let chooseHypothesises trainingDatum hypothesises =
+    let chooseClassifiers trainingDatum classifiers =
+        /// 訓練データの件数
+        let trainingDatumCount = trainingDatum |> List.length
+        /// 重みの初期リスト
         let initialDistributions = seq { for _ in 1 .. trainingDatumCount -> 1.0 / float trainingDatumCount }
-        (initialDistributions, hypothesises) |> Seq.unfold (fun (distributions, hypothesises) ->
-            match getBestHypothesis trainingDatum distributions hypothesises with
+        // エラー率の低い弱分類器を順に抽出したリストを作成する
+        initialDistributions |> Seq.unfold (fun distributions ->
+            match getBestClassifier trainingDatum distributions classifiers with
             | _, error when error >= 0.5 -> None
-            | bestHypothesis, error ->
-                let weight = getWeightOfHypothesis error
-                let nextDistributions = getNextDistributions bestHypothesis weight trainingDatum distributions
-                let nextHypothesises = hypothesises |> Seq.filter ((<>) bestHypothesis)
-                Some((bestHypothesis, weight), (nextDistributions, nextHypothesises)))
+            | bestClassifier, error ->
+                let weight = getWeightOfClassifier error
+                let nextDistributions = getNextDistributions bestClassifier weight trainingDatum distributions
+                Some((bestClassifier, weight), nextDistributions))
         |> Seq.toList
+
+    /// 弱分類器の並列処理用シーケンス
+    let weakClassifiers = weakClassifiers |> List.toSeq |> AsyncSeq.ofSeq
 
     // 仮説を返す
     fun queryData ->
-        chooseHypothesises trainingDatum weakHypothesises |> List.sumBy (fun (hypothesis, weight) ->
-            hypothesis.Evaluate queryData |> Label.ToInt |> float |> (*) weight)
+        chooseClassifiers trainingDatum weakClassifiers |> List.sumBy (fun (classifier, weight) ->
+            classifier queryData |> Label.ToInt |> float |> (*) weight)
         |> sign |> Label.CreateFromSign (Label.Create 1)
