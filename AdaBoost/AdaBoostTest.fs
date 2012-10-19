614cdd73f3eacae4226947da9eb6125e6bdd49ab
adacola <adacola@le.to>
2012-10-19 15:50:53 +0900

    関数名を変更

--- AdaBoost/AdaBoostTest.fs
+++ AdaBoost/AdaBoostTest.fs
@@ -28,23 +28,23 @@ let draw (fileName : string) (width : int) (height : int) shapes =
     shapes |> Seq.iter (fun (shape : Shape) -> shape.Draw canvas)
     bitmap.Save(fileName, Imaging.ImageFormat.Png)
 
-let getPointComparisons max = seq {
+let getPointClassifiers max = seq {
     for elementStr, elementFunc in ["x", fst; "y", snd] do
     for comparerStr, comparerFunc in ["<", (>); ">", (<)] do
     for c in 0 .. max ->
         { Name = sprintf "%s %s %d" elementStr comparerStr c; Classify = elementFunc >> comparerFunc c >> Label.FromBool } }
 
-let getDistanceComparisons max points = seq {
+let getDistanceClassifiers max points = seq {
     let points = points |> Seq.toArray
     for i in 0 .. points.Length - 1 do
     for r in 1 .. max ->
         { Name = sprintf "distance to points.[%d] < %d" i r; Classify = distanceSquare points.[i] >> (>) r >> Label.FromBool } }
 
-let getCosines point1 point2 = seq {
+let getCosineClassifiers point1 point2 = seq {
     let (x1, y1), (x2, y2) = point1, point2
     let norm (x, y) = x * x + y * y |> float |> sqrt
     let getCosine (x, y) =
         let (x1', y1'), (x2', y2') = (x1 - x, y1 - y), (x2 - x, y2 - y)
         float (x1' * x2' + y1' * y2') / (norm (x1', y1') * norm (x2', y2'))
     for t in -1. .. 0.01 .. 1. ->
-        { Name = sprintf "cos θ ≒ %f" t; Classify = getCosine >> (fun cosθ -> t - 0.005 <= cosθ && cosθ < t + 0.005) >> Label.FromBool } }
+        { Name = sprintf "cos θ < %f" t; Classify = getCosine >> (>) t >> Label.FromBool } }
